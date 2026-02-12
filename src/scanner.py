ab3f58f2af2e980c50dd8d6c9e2e5dcf8092a7e9
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-12 15:43:30 +0300

    feat: added the ability to give localhost instead of 127.0.0.1:w

--- src/scanner.py
+++ src/scanner.py
@@ -1,10 +1,17 @@
 import socket
 
 def scan_given_port(ip: str, port: int) -> bool:
+    ip_used: str
+
+    if ip == 'localhost':
+        ip_used = '127.0.0.1'
+    else:
+        ip_used = ip
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(1)
         try:
-            s.connect((ip, port))
+            s.connect((ip_used, port))
             return True
         except ConnectionRefusedError:
             return False
