c7c209fbc57cb5fd187a1b28163559d6e7e349e2
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-12 14:55:54 +0300

    test: created a test for scan_given_port function

---
+++ tests/test_scan_given_port.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+
+sys.path.insert(1, 'src')
+
+import scanner
+
+class TestScanGivenPort(unittest.TestCase):
+
+    def test_open_port(self):
+        self.assertEqual(scanner.scan_given_port('', 53), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
