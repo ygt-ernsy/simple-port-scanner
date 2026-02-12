ce9b5d0342e8fe02b3e3e21e5c70ff9679727427
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-12 15:38:01 +0300

    test: created a closed port tets

--- tests/test_scan_given_port.py
+++ tests/test_scan_given_port.py
@@ -10,6 +10,8 @@ class TestScanGivenPort(unittest.TestCase):
     def test_open_port(self):
         self.assertEqual(scanner.scan_given_port('', 53), True)
 
+    def test_closed_port(self):
+        self.assertEqual(scanner.scan_given_port('', 9999), False)
 
 if __name__ == '__main__':
     unittest.main()
