89fd34aec58f9043ea745b5c17b568587dba49ef
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-17 21:10:18 +0300

    test: created the initial test cases for ip scanner functions

---
+++ tests/test_scan_ip_adress_sequential.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+
+sys.path.insert(1, 'src')
+
+import scanner
+
+class TestScanIpAdressSequential(unittest.TestCase):
+
+    def test_local_host(self):
+        pass
+
+    # TODO : Find a way impelent the test
+
+    # TODO : Create more tests
+
+if __name__ == '__main__':
+    unittest.main()
