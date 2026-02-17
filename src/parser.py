a42e221474601db857e3008ebf00a065df7ca41b
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-17 21:04:09 +0300

    feat: added the ability to give an ip address to scan for in parser.py and the ability to specify the scan to be concurrent

--- src/parser.py
+++ src/parser.py
@@ -1,18 +1,34 @@
 import argparse
-from scanner import scan_given_port
+from scanner import scan_concurent_ip, scan_given_port, scan_sequential_ip
 
 parser = argparse.ArgumentParser(
     prog='Port scanner',
-    description='',
-    epilog=''
+    description='Scans for open ports on a target IP.',
+    epilog='Example: python scanner.py -sT localhost --concurrent'
 )
 
-parser.add_argument('-sT', nargs=2, help='')
+parser.add_argument('-sT', nargs='+', help='Target IP and optional Port (e.g., "127.0.0.1" or "127.0.0.1 80")', required=True)
+parser.add_argument('--concurent', action='store_true', help='Use multi-threading for faster scanning')
 
 args = parser.parse_args()
 
-if args.sT:
-    if scan_given_port(args.sT[0], int(args.sT[1])):
+target_ip = args.sT[0]
+
+if len(args.sT) == 2:
+    target_port: int = int(args.sT[1])
+    print(f"Scanning single port {target_port} on {target_ip}...")
+    if scan_given_port(target_ip, target_port):
         print('open')
     else:
         print('closed')
+
+elif len(args.sT) == 1:
+    if args.concurent:
+        found = scan_concurent_ip(target_ip)
+    else:
+        found = scan_sequential_ip(target_ip)
+    print(f"\nScan complete. Open ports: ")
+    for port in found:
+        print(port)
+else:
+    print("Error: Too many arguments for -sT. Please provide IP, or IP and Port.")
