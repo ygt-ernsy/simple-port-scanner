19a5dbd954dbaddf2826f607c961466d5f6df340
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-17 19:20:18 +0300

    feat: created a function that scans the ports of a given ip address using threads

--- src/scanner.py
+++ src/scanner.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import socket
 from typing import List
 
@@ -28,4 +29,22 @@ def scan_given_ip(ip: str) -> List[str]:
     for i in range(65536):
         if scan_given_port(ip, i):
             open_ports.append(f'{ip}:{i}')
+    return open_ports
+
+def scan_ip(ip: str) -> List[str]:
+    open_ports: List[str] = []
+    futures: dict[concurrent.futures.Future, str] = {}
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+        for i in range(65536):
+            port_address = f"{ip}:{i}" 
+            futures[executor.submit(scan_given_port, ip, i)] = port_address
+
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                if future.result():
+                    open_ports.append(str(futures.get(future)))
+            except Exception as e:
+                print(e)
+
     return open_ports
