58dea32d47de9c1d9d0bacc6574ca6904f0fdb93
ygt-ernsy <yigiterensoy@hotmail.com>
2026-02-23 21:48:18 +0300

    feat: added the ability to get service of a port to the parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,5 @@
 import argparse
-from scanner import scan_concurent_ip, scan_given_port, scan_sequential_ip
+from scanner import *
 
 parser = argparse.ArgumentParser(
     prog='Port scanner',
@@ -7,28 +7,38 @@ parser = argparse.ArgumentParser(
     epilog='Example: python scanner.py -sT localhost --concurrent'
 )
 
-parser.add_argument('-sT', nargs='+', help='Target IP and optional Port (e.g., "127.0.0.1" or "127.0.0.1 80")', required=True)
-parser.add_argument('--concurent', action='store_true', help='Use multi-threading for faster scanning')
+parser.add_argument('-sT', nargs='+', help='Target IP and optional Port (e.g., "127.0.0.1" or "127.0.0.1 80")')
+parser.add_argument('--concurent', action='store_true', help='Use multi-threading for faster scanning. Note: Only supports -sT for now')
+parser.add_argument('-gS', help='Gets the service of the given port') # The should be thought on further
 
 args = parser.parse_args()
 
-target_ip = args.sT[0]
+if args.sT:
+    target_ip = args.sT[0]
 
-if len(args.sT) == 2:
-    target_port: int = int(args.sT[1])
-    print(f"Scanning single port {target_port} on {target_ip}...")
-    if scan_given_port(target_ip, target_port):
-        print('open')
-    else:
-        print('closed')
+    if len(args.sT) == 2:
+        target_port: int = int(args.sT[1])
+        print(f"Scanning single port {target_port} on {target_ip}...")
+        if scan_given_port(target_ip, target_port):
+            print('open')
+        else:
+            print('closed')
 
-elif len(args.sT) == 1:
-    if args.concurent:
-        found = scan_concurent_ip(target_ip)
+    elif len(args.sT) == 1:
+        if args.concurent:
+            found = scan_concurent_ip(target_ip)
+        else:
+            found = scan_sequential_ip(target_ip)
+        print(f"\nScan complete. Open ports: ")
+        for port in found:
+            print(port)
     else:
-        found = scan_sequential_ip(target_ip)
-    print(f"\nScan complete. Open ports: ")
-    for port in found:
-        print(port)
-else:
-    print("Error: Too many arguments for -sT. Please provide IP, or IP and Port.")
+        print("Error: Too many arguments for -sT. Please provide IP, or IP and Port.")
+elif args.gS:
+    target_port: int = int(args.gS)
+    service: str = get_service_of_port(target_port)
+
+    if service != '':
+        print(f"The service of the port {target_port} is {service}.")
+    else:
+        print(f"The service of the port {target_port} is unknown.")
